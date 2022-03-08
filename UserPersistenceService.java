090447012b3eae15273dd2e248fb3988d7808361
Juan <a2209872@correo.uia.mx>
2022-03-07 22:39:08 -0600

    ejercicio

---
+++ UserPersistenceService.java
@@ -0,0 +1,9 @@
+package org.sisoftware;
+
+public class UserPersistenceService {
+    private Store store = new Store();
+
+    public void saveUser(User user){
+        store.store(user);
+    }
+}
